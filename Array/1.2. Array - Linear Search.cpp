1440fbda42407ed920ea82525d3cdfbf82e79ca0
Numan Ahmed <mdnmnahmed@gmail.com>
2020-11-01 19:43:27 +0530

    Array-Linear Search

---
+++ Array/1.2. Array - Linear Search.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int n, int key){
+	for(int i=0; i<n; i++){
+		if(arr[i] == key){
+			cout<<endl<<"\t"<<key<<" Founded at the Position of: "<<i+1;
+			return i+1;
+		}
+	}
+	
+	cout<<endl<<key<<" Not Found...";
+	return -1;
+}
+
+int main(){
+	
+	int n, arr[n];
+	
+	cout<<"How many Elements u want to Enter: ";
+	cin>>n;
+	
+	cout<<endl;
+	for(int i=0; i<n; i++){
+		cout<<"Enter "<<i+1<<" th Number:- ";
+		cin>>arr[i];
+	}
+	
+	cout<<"\nEnter the Number u want to Search: ";
+	int key;
+	cin>>key;
+	
+	linearSearch(arr, n, key);
+
+    return 0;
+}   
