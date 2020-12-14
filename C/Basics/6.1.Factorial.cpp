3c2dbf14b70eaa744deb5c9e3bebf740503ef33f
Numan Ahmed <mdnmnahmed@gmail.com>
2020-12-14 11:45:38 +0530

    C Basics practice

---
+++ C/Basics/6.1.Factorial.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num;
+    cout<<"Enter a Number to print it's Factorial: ";
+	cin>>num;
+	
+	int factorial = 1;
+	
+	for(int i=num; i>1; i--){
+		factorial *= i;
+	}
+	
+	cout<<"Factorial of: "<<num<<" is:- "<<factorial;
+    
+    return 0;
+}   
