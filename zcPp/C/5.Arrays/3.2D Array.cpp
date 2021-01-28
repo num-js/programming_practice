78282cd21f88e9dae1ded067a193972d63972591
Numan Ahmed <mdnmnahmed@gmail.com>
2021-01-29 04:21:05 +0530

    Learning 2D Array

---
+++ zcPp/C/5.Arrays/3.2D Array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int nStdnt = 3;
+	int nSubjct = 4;
+	
+	int marks[nStdnt][nSubjct];
+
+	for(int row=0; row<nStdnt; row++){
+		for(int col=0; col<nSubjct; col++){
+			cout<<"Student No. "<<row+1<<" Enter Marks of Subject No. "<<col+1<<" : ";
+			cin>>marks[row][col];
+		}
+	}
+
+
+	for(int row=0; row<nStdnt; row++){
+		for(int col=0; col<nSubjct; col++){
+			cout<<"Student No. "<<row+1<<" Marks of Subject No. "<<col+1<<" is : "<<marks[row][col]<<endl;
+		}
+	}
+	
+	
+	return 0;
+}
