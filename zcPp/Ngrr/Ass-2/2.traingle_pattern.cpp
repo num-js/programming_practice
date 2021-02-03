6c0523628b6771d79ca916199fd58b42d2617dfb
Numan Ahmed <mdnmnahmed@gmail.com>
2021-02-03 18:25:33 +0530

    cpp

--- zcPp/Ngrr/Ass-2/2.traingle_pattern.cpp
+++ zcPp/Ngrr/Ass-2/2.traingle_pattern.cpp
@@ -3,35 +3,34 @@ using namespace std;
 int main() 
 {
     int n;
+    
+	cout<<"Enter Height of the Traingle: ";
     cin>>n;
-    int row=1,i,val=1,number=1;
-    while(row<=n)
-    {
+    
+	int row=1,i,val=1,number=1;
+    
+	while(row<=n) {
         int spaces=1;
-        while(spaces<=n-row)
-        {
+        while(spaces<=n-row) {
             cout<<"  ";
             spaces=spaces+1;
         }
-        for(int i=row;i<=2*row-1;i++)
-        {
+        
+        for(int i=row;i<=2*row-1;i++) {
             cout<<i<<" ";
+        }
 
-        }
-        for(int i=2*row-2;i>=row;i--)
-        {
+        for(int i=2*row-2;i>=row;i--) {
             cout<<i<<" ";
         }
-        spaces=1;
-        while(spaces<=n-row)
-        {
+        
+		spaces=1;
+        while(spaces<=n-row) {
             cout<<" ";
             spaces=spaces+1;
         }
 
         cout<<endl;
         row=row+1;
-
     }
-    
 }
