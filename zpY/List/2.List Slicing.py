ec50f081ec7fb2891eb12e209ca5172efd3511ce
Numan Ahmed <mdnmnahmed@gmail.com>
2021-02-14 08:20:44 +0530

    List slicing in pY

--- zpY/List/2.List Slicing.py
+++ zpY/List/2.List Slicing.py
@@ -1,7 +1,5 @@
-list1 = [1, 5, 2, 7]
-list1[0] = True;
-# print(list1);
+myFrnds = ['Num', 'Saheb', 'Ujjual', 'Gopal'];
 
-
-list2 = [1, "Num", 2, True, 40.6]
-# print(list2);  
+print(myFrnds);
+print(myFrnds[1:3]);
+print(myFrnds[:-2]);
