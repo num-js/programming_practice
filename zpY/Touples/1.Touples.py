080cccc0f704fb5b4fd6c1080e7cfd03cc3d5b27
Numan Ahmed <mdnmnahmed@gmail.com>
2021-02-14 08:44:56 +0530

    Touples in pY

---
+++ zpY/Touples/1.Touples.py
@@ -0,0 +1,13 @@
+touple1 = (1, 3, 9, 2)
+
+
+# touple1[0] = 5 # Wrong, Touple is immutable
+
+tpl1 = () #Empty Touple
+print(tpl1)
+
+tpl2 = (1) #Wrong, touple need atleast 1 coma for
+tpl2 = (1,) #touple with single Element
+print(tpl2)
+
+
