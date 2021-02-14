a28992a144519c1cc944ab50240a29ee44e8e75a
Numan Ahmed <mdnmnahmed@gmail.com>
2021-02-14 08:35:33 +0530

    List Methods

---
+++ zpY/List/3.List-Methods.py
@@ -0,0 +1,24 @@
+list1 = [5, 3, 1, 9, 1.5, 8];
+# list1 = ['num', 'Ahmed', 'ahmed', 'numan'];
+print(list1);
+
+    #***Shorting***#
+# list1.sort()
+
+    #***Reverse***#
+# list1.reverse()
+
+    #***Append***#
+# list1.append(50)
+
+    #***Insert***#  --- (index, value)
+# list1.insert(1, 30)
+
+    #***Pop***# --- Remove element at the index (0)
+# list1.pop(1) 
+
+    #***remove***# --- Remove the element
+list1.remove(1.5)
+
+
+print(list1);
