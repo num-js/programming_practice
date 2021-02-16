15f9e865322e33cb08b55e85b02d3a56a8bfc497
Numan Ahmed <mdnmnahmed@gmail.com>
2021-02-16 18:10:54 +0530

    swap 2 numbers

---
+++ zpY/zPrbLmS/4.Swap 2 Numbers.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 20
+
+print('Before Swap')
+print(num1)
+print(num2)
+
+num1 = num1 + num2
+num2 = num1 - num2
+num1 = num1 - num2
+
+print('After Swap')
+print(num1)
+print(num2)
