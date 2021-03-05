462121c227ace86432881187d8c50f3908b77f05
Numan Ahmed <mdnmnahmed@gmail.com>
2021-03-05 14:59:06 +0530

    serialize Array in JS

---
+++ jSp/4.serialize-Array.js
@@ -0,0 +1,8 @@
+
+const myArr = ["Numan", "Full Stack Dev", "JS & PY"];
+
+
+const serializeArr = JSON.stringify(myArr);
+
+console.log('typeOf: ', typeof(serializeArr));
+console.log('serializeArr: ', serializeArr);
