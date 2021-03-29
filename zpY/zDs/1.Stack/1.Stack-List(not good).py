1b58c917d73a6d7be1eaa3d77346b75a6174544e
Numan Ahmed <mdnmnahmed@gmail.com>
2021-03-29 09:00:54 +0530

    Stack DS with List Array in Py

---
+++ zpY/zDs/1.Stack/1.Stack-List(not good).py
@@ -0,0 +1,16 @@
+aStack = []
+
+    # Adding Elements at Last
+aStack.append('Numan');
+aStack.append('Ahmed');
+aStack.append('7001959252');
+aStack.append('Full Stack JS');
+
+print(aStack)
+
+    # Removing Elements from Last
+aStack.pop()
+aStack.pop()
+
+
+print(aStack)
