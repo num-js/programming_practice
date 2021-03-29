e23bdb743d50b8b084512df1ab07007c0eb6f682
Numan Ahmed <mdnmnahmed@gmail.com>
2021-03-29 09:01:35 +0530

    Stack with Dequeue

---
+++ zpY/zDs/1.Stack/2.Stack-Dequeue.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+myStack = deque()
+
+    # Appending at Lsat
+myStack.append('Numan')
+myStack.append('Ahmed')
+myStack.append(7001959252)
+myStack.append('Full Stack')
+
+print(myStack)
+
+    # Removing from Last
+myStack.pop()
+
+print(myStack)
