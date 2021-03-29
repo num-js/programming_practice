b02eddd3228f8f7ca312244de7819e0e592d9897
Numan Ahmed <mdnmnahmed@gmail.com>
2021-03-29 09:02:03 +0530

    Date in JS

---
+++ jSp/problems/Date/1.Date.js
@@ -0,0 +1,23 @@
+const currentFullDate = new Date();
+// console.log(currentFullDate);
+
+const currentYear = currentFullDate.getFullYear();
+// console.log(currentYear);
+
+const currentMonth = currentFullDate.getMonth();
+// console.log(currentMonth+1);
+
+const currentDay = currentFullDate.getDate();
+// console.log(currentDay);
+
+const currentHour = currentFullDate.getHours();
+// console.log(currentHour);
+
+const currentMin = currentFullDate.getMinutes();
+// console.log(currentMin);
+
+const currentSec = currentFullDate.getSeconds();
+// console.log(currentSec);
+
+const currentMiliSec = currentFullDate.getMilliseconds();
+console.log(currentMiliSec);
